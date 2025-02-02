05ea5981b5f23073b3d7c3ca5d13c855c2380508
Alexander Chasovskoy <a.n.chasovskoy@gmail.com>
2025-02-02 18:46:35 +0100

    basic product model is added

--- apps/products/models.py
+++ apps/products/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+from apps.accounts.models import CustomUser
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    supplier = models.ForeignKey(
+        CustomUser, on_delete=models.CASCADE, limit_choices_to={"role": "shop"}
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
